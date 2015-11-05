9bb1f06154fb7975abbff0f3a0f32979639ac23d
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2015-11-06 00:02:04 +0800

    clean up middleware

--- middleware/api.js
+++ middleware/api.js
@@ -1,4 +1,3 @@
-import { Schema, arrayOf, normalize } from 'normalizr';
 import { camelizeKeys } from 'humps';
 import superAgent from 'superagent';
 import Promise from 'bluebird';
@@ -6,7 +5,12 @@ import _ from 'lodash';
 
 export const CALL_API = Symbol('CALL_API');
 
-function createRequest ({ getState, next, request }) {
+export default store => next => action => {
+  if ( ! action[CALL_API] ) {
+    return next(action);
+  }
+  let request = action[CALL_API];
+  let { getState } = store;
   let deferred = Promise.defer();
   // handle 401 and auth here
   let { method, url, successType } = request;
@@ -27,14 +31,4 @@ function createRequest ({ getState, next, request }) {
     });
 
   return deferred.promise;
-}
-
-export default store => next => action => {
-  if ( ! action[CALL_API] ) {
-    return next(action);
-  }
-  let request = action[CALL_API];
-  let { getState } = store;
-
-  return createRequest({ getState, next, request });
 };
