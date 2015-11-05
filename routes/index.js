07d2dc73701329260b5817ca18cba3b92787f2b8
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2015-11-06 00:27:06 +0800

    clean up

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,12 @@ import configureStore from 'store/configureStore';
 import App from 'containers/App';
 import Intro from 'containers/Intro';
 import Question from 'containers/Question';
-import UserDataPage from 'containers/UserDataPage';
 
 export default function(history) {
   return (
     <Router history={history}>
       <Route path="/" component={App}>
         <Route path="/q/:questionId/:questionTitle" component={Question} />
-        <Route path="/testing/:username" component={UserDataPage} />
         <IndexRoute component={Intro} />
       </Route>
     </Router>
