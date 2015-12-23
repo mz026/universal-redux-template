a420113f3d7cb8eae0484c1566f3f1975b643d43
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2015-12-23 15:58:47 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 var path = require('path');
 var webpack = require('webpack');
+var AssetsPlugin = require('assets-webpack-plugin');
 
-module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+var DEBUG = !(process.env.NODE_ENV === 'production');
+
+var config = {
+  devtool: DEBUG ? 'cheap-module-eval-source-map' : false,
   entry: [
-    'webpack-dev-server/client?http://localhost:3001',
-    'webpack/hot/only-dev-server',
     './app'
   ],
   resolve: {
@@ -13,38 +14,59 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: 'http://localhost:3001/static/'
+    filename: DEBUG ? 'bundle.js' : 'bundle.[hash].js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    })
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/,
-      include: __dirname,
-      query: {
-        optional: ['runtime'],
-        stage: 2,
-        env: {
-          development: {
-            plugins: [
-              'react-transform'
-            ],
-            extra: {
-              'react-transform': {
-                'transforms': [{
-                  'transform':  'react-transform-hmr',
-                  imports: ['react'],
-                  locals:  ['module']
-                }]
-              }
-            }
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+};
+
+
+if (DEBUG) {
+  config.entry = [
+    'webpack-dev-server/client?http://localhost:3001',
+    'webpack/hot/only-dev-server',
+  ].concat(config.entry);
+
+  config.plugins = config.plugins.concat([ new webpack.HotModuleReplacementPlugin() ]);
+  config.output.publicPath = 'http://localhost:3001/static/';
+  config.module.loaders[0].query = {
+    optional: ['runtime'],
+    stage: 2,
+    env: {
+      development: {
+        plugins: [
+          'react-transform'
+        ],
+        extra: {
+          'react-transform': {
+            'transforms': [{
+              'transform':  'react-transform-hmr',
+              imports: ['react'],
+              locals:  ['module']
+            }]
           }
         }
       }
-    }]
-  }
-};
+    }
+  };
+} else {
+  config.plugins = config.plugins.concat([
+    new webpack.optimize.UglifyJsPlugin(),
+    new AssetsPlugin({ path: path.join(__dirname, 'dist') })
+  ]);
+}
+
+module.exports = config;
