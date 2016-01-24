a6b6c7539892a6b008b2114ec312fda96c6db123
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-24 16:10:28 +0800

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,9 @@ var gulp = require('gulp'),
 gulp.task('css', function() {
   return gulp.src('styles/main.scss')
            .pipe($.sass().on('error', $.sass.logError))
+           .pipe($.autoprefixer({
+             browsers: ['last 2 versions'],
+             cascade: false
+           }))
            .pipe(gulp.dest('./.tmp'));
 })
