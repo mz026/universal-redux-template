202b7317b99d3a9f8ef674f59a6f80b1c9c5d284
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-24 16:12:33 +0800

    add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var gulp = require('gulp'),
 
 gulp.task('css', function() {
   return gulp.src('styles/main.scss')
+           .pipe($.sourcemaps.init())
            .pipe($.sass().on('error', $.sass.logError))
            .pipe($.autoprefixer({
              browsers: ['last 2 versions'],
              cascade: false
            }))
+           .pipe($.sourcemaps.write())
            .pipe(gulp.dest('./.tmp'));
 })
