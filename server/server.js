788043a601cf8769e489bf86fdc44e4b6312c3dc
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-02-11 22:09:33 +0800

    fix express warning
    - update send(statusCode, response) to status(statusCode).send(response)

--- server/server.js
+++ server/server.js
@@ -56,11 +56,11 @@ server.get('*', (req, res)=> {
 
   match({ routes, location }, (error, redirectLocation, renderProps) => {
     if (redirectLocation) {
-      res.redirect(301, redirectLocation.pathname + redirectLocation.search)
+      res.redirect(301, redirectLocation.pathname + redirectLocation.search);
     } else if (error) {
-      res.send(500, error.message)
+      res.status(500).send(error.message);
     } else if (renderProps == null) {
-      res.send(404, 'Not found')
+      res.status(404).send('Not found')
     } else {
       let [ getCurrentUrl, unsubscribe ] = subscribeUrl();
       let reqUrl = location.pathname + location.search;
