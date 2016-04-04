3f5c0363af7783c5168f45fec6b1a036a7cc8c32
Tylor Shin <shincode@icloud.com>
2016-04-04 22:01:12 +0900

    Upgrade bluebird package version && Changed deprecated method to make Promise object

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -1,32 +1,53 @@
 import superAgent from 'superagent';
 import Promise from 'bluebird';
 import _ from 'lodash';
+import config from 'config';
 
 export const CALL_API = Symbol('CALL_API');
 
 export default store => next => action => {
-  if ( ! action[CALL_API] ) {
+  if (!action[CALL_API]) {
     return next(action);
   }
-  let request = action[CALL_API];
-  let { getState } = store;
-  let deferred = Promise.defer();
-  let { method, url, successType } = request;
-  superAgent[method](url)
-    .end((err, res)=> {
-      if ( !err ) {
-        next({
-          type: successType,
-          response: res.body
+
+  const request = action[CALL_API];
+  const { getState } = store;
+  const { method, path, successType, body } = request;
+  const url = `${config.API_BASE_URL}${path}`;
+
+  if (!!body) {
+    return new Promise((resolve) => {
+      superAgent[method](url)
+        .send(body)
+        .end((err, res) => {
+          if (!err) {
+            next({
+              type: successType,
+              response: res.body
+            });
+
+            if (_.isFunction(request.afterSuccess)) {
+              request.afterSuccess({ getState });
+            }
+          }
+          resolve();
         });
-
-        if (_.isFunction(request.afterSuccess)) {
-          request.afterSuccess({ getState });
-        }
-
-      }
-      deferred.resolve();
     });
+  }
+  return new Promise((resolve) => {
+    superAgent[method](url)
+      .end((err, res) => {
+        if (!err) {
+          next({
+            type: successType,
+            response: res.body
+          });
 
-  return deferred.promise;
+          if (_.isFunction(request.afterSuccess)) {
+            request.afterSuccess({ getState });
+          }
+        }
+        resolve();
+      });
+  });
 };
