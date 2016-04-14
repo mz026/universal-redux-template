01c9860e80537e8cc4f927577c570be97b86f56b
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-04-14 18:23:35 +0800

    add chainApi middleware

--- app/middleware/api.js
+++ app/middleware/api.js
@@ -1,5 +1,5 @@
 import superAgent from 'superagent';
-import Promise from 'bluebird';
+import Promise, { using } from 'bluebird';
 import _ from 'lodash';
 import config from 'config';
 
@@ -43,3 +43,82 @@ export default store => next => action => {
 
   return deferred.promise;
 };
+
+function extractParams (callApi) {
+  let { method, path, successType, errorType, afterSuccess, afterError } = callApi;
+  let url = `${config.API_BASE_URL}${path}`
+
+  return {
+    method,
+    url,
+    successType,
+    errorType,
+    afterSuccess,
+    afterError
+  }
+}
+
+export const CHAIN_API = Symbol('CHAIN_API')
+let apiChain = ({ dispatch, getState }) => next => action => {
+  let deferred = Promise.defer()
+
+  if (! action[CHAIN_API]) {
+    return next(action)
+  }
+
+  let promiseCreators = action[CHAIN_API].map((apiActionCreator)=> {
+    return createRequestPromise(apiActionCreator, next, getState, dispatch)
+  })
+
+  let overall = promiseCreators.reduce((promise, creator)=> {
+    return promise.then((body)=> {
+      return creator(body)
+    })
+  }, Promise.resolve())
+
+  overall.finally(()=> {
+    deferred.resolve()
+  }).catch(()=> {})
+
+  return deferred.promise
+}
+
+function createRequestPromise (apiActionCreator, next, getState, dispatch) {
+  return (prevBody)=> {
+    let apiAction = apiActionCreator(prevBody)
+    let deferred = Promise.defer()
+    let params = extractParams(apiAction[CALL_API])
+
+    superAgent[params.method](params.url)
+      .end((err, res)=> {
+        if (err) {
+          if ( params.errorType ) {
+            dispatch({
+              type: params.errorType,
+              error: err
+            })
+          }
+
+          if (_.isFunction(params.afterError)) {
+            params.afterError({ getState });
+          }
+          deferred.reject();
+        } else {
+          dispatch({
+            type: params.successType,
+            response: res.body
+          });
+
+          if (_.isFunction(params.afterSuccess)) {
+            params.afterSuccess({ getState });
+          }
+          deferred.resolve(res.body);
+        }
+      });
+
+    return deferred.promise
+  }
+
+}
+
+export { apiChain }
--- app/spec/middleware/api.test.js
+++ app/spec/middleware/api.test.js
@@ -1,6 +1,8 @@
 import nock from 'nock';
-import apiMiddleware, { CALL_API } from 'middleware/api';
+import apiMiddleware, { CALL_API, CHAIN_API, apiChain } from 'middleware/api';
 import config from 'config';
+
+import superagent from 'superagent'
 
 describe('Middleware::Api', function(){
   let store, next;
@@ -9,7 +11,8 @@ describe('Middleware::Api', function(){
   let path = '/the-url/path';
 
   beforeEach(function(){
-    store = {};
+    store = {
+    };
     next = sinon.stub();
     action = {
       [CALL_API]: {
@@ -19,6 +22,154 @@ describe('Middleware::Api', function(){
       }
     };
   });
+
+  describe.only('`apiChain` middleware', function(){
+    let path1 = '/the-url/path-1'
+    let successType1 = 'ON_SUCCESS_1'
+    let successType2 = 'ON_SUCCESS_2'
+    let errorType2 = 'ON_ERROR_2'
+
+    let nockScope1, nockScope2
+
+    let afterSuccess1, afterSuccess2
+    let response1 = { id: 'the-id-1' }
+    let response2 = { id: 'the-res-2' }
+
+    let afterError2
+    let afterError
+
+    beforeEach(function(){
+      store = {
+        dispatch: sinon.spy(),
+        getState: sinon.spy()
+      }
+      afterSuccess1 = sinon.stub()
+      afterSuccess2 = sinon.stub()
+      afterError2 = sinon.stub()
+      action = {
+        [CHAIN_API]: [
+          ()=> {
+            return {
+              extra1: 'val1',
+              [CALL_API]: {
+                method: 'get',
+                path: path1,
+                afterSuccess: afterSuccess1,
+                successType: successType1
+              }
+            }
+          },
+          (resBody1)=> {
+            return {
+              extra2: 'val2',
+              [CALL_API]: {
+                method: 'get',
+                path: `/the-url/${resBody1.id}`,
+                afterSuccess: afterSuccess2,
+                afterError: afterError2,
+                successType: successType2,
+                errorType: errorType2
+              }
+            }
+          }
+        ]
+      }
+    })
+
+    afterEach(function(){
+      nock.cleanAll();
+    })
+    describe('when all API calls are success', function(){
+      beforeEach(function(){
+        nockScope1 = nock(config.API_BASE_URL).get(path1).reply(200, response1)
+        nockScope2 = nock(config.API_BASE_URL).get('/the-url/the-id-1')
+                                              .reply(200, response2)
+      })
+
+      it('sends requests to all endpoints', function(done){
+        let promise = apiChain(store)(next)(action)
+
+        promise.then(()=> {
+          nockScope1.done()
+          nockScope2.done()
+          done()
+        })
+      })
+      it('trigger afterSuccess for all endpoints', function(done){
+        let promise = apiChain(store)(next)(action)
+        promise.then(()=> {
+          expect(afterSuccess1).to.have.been.calledWith({ getState: store.getState})
+          expect(afterSuccess2).to.have.been.calledWith({ getState: store.getState})
+          done()
+        })
+      })
+      it('dispatch successType for all endpoints', function(done){
+        let promise = apiChain(store)(next)(action)
+        promise.then(()=> {
+          expect(store.dispatch).to.have.been
+            .calledWith({ type: successType1, response: response1 })
+          expect(store.dispatch).to.have.been
+            .calledWith({ type: successType2, response: response2 })
+          done()
+        })
+      })
+    })
+
+    describe('when one of the apis failed', function(){
+      beforeEach(function(){
+        nockScope1 = nock(config.API_BASE_URL).get(path1).reply(200, { id: 'the-id-1' })
+        nockScope2 = nock(config.API_BASE_URL).get('/the-url/the-id-1')
+                                              .reply(400, { id: 'the-res-2' })
+      })
+      it("sends request until it's failed", function(done){
+        let promise = apiChain(store)(next)(action)
+        promise.then(()=> {
+          nockScope1.done()
+          nockScope2.done()
+          done()
+        })
+      })
+      it('triggers afterSuccess and dispatches success for the ok ones', function(done){
+        let promise = apiChain(store)(next)(action)
+        promise.then(()=> {
+          expect(store.dispatch).to.have.been.calledWith({
+            type: successType1,
+            response: response1
+          })
+          expect(afterSuccess1).to.have.been.calledWith({ getState: store.getState })
+          done()
+        })
+      })
+      it('trigger afterError of path2', function(done){
+        let promise = apiChain(store)(next)(action)
+        promise.then(()=> {
+          expect(afterError2).to.have.been.calledWith({ getState: store.getState})
+          done()
+        })
+      })
+      it('dispatches errorType of path2', function(done){
+        let dispatchedAction
+        store.dispatch = function(a) {
+          dispatchedAction = a
+        }
+        let promise = apiChain(store)(next)(action)
+        promise.then(()=> {
+          expect(dispatchedAction.type).to.equal(errorType2)
+          expect(dispatchedAction.error).to.be.an.instanceOf(Error)
+          done()
+        })
+      })
+    })
+  })
+
+
+
+
+
+
+
+
+
 
   describe('when action is without CALL_API', function(){
     it('passes the action to next middleware', function(){
