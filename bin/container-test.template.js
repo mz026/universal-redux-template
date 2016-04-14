a2acf7febe49adcfa26d084fef85b78c2c35379d
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-04-14 23:57:16 +0800

    update code template

--- bin/container-test.template.js
+++ bin/container-test.template.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
-import rewire from 'rewire'
-import reactMocker from 'test/utils/reactMocker'
+import Container, { COMPONENT_NAME } from 'containers/PATH_PREFIX/COMPONENT_NAME'
 
 describe.only('Container::COMPONENT_FULL_NAMESPACE', function(){
   let props
-  let Container, COMPONENT_NAME
-  beforeEach(function(){
-    Container = rewire('containers/PATH_PREFIX/COMPONENT_NAME')
-    COMPONENT_NAME = Container.COMPONENT_NAME
-  })
 
   function renderDoc () {
     return TestUtils.renderIntoDocument(<COMPONENT_NAME {...props}/>)
